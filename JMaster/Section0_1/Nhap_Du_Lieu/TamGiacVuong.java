249a16645705175d70e003a5cac0b86007943c48
Minh Thu Tran Thuy <88418499+tranthuyminhthu@users.noreply.github.com>
2022-01-12 22:00:47 +0700

    JMaster

---
+++ JMaster/Section0_1/Nhap_Du_Lieu/TamGiacVuong.java
@@ -0,0 +1,24 @@
+package Section0_1;
+
+public class TamGiacVuong {
+	
+	/**
+	 * 
+	 * @param e : canh goc vuong
+	 * @param f : canh goc vuong
+	 * @param g : duong cao tam giac vuong
+	 * @return
+	 * 
+	 */
+	public static double chuViTamGiacVuong (double e, double f, double g) {
+		return e + f + g;
+		
+	}
+	
+	public static double dienTichTamGiacVuong(double e, double f) {
+		
+		return 0.5*e*f;
+		
+	}
+	      
+}
