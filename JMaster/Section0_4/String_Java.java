ea02d6fddfcd7a6bb689ff79c54f504e079bb62c
Minh Thu Tran Thuy <88418499+tranthuyminhthu@users.noreply.github.com>
2022-01-26 12:29:48 +0700

    JMaster

---
+++ JMaster/Section0_4/String_Java.java
@@ -0,0 +1,42 @@
+package Section0_4;
+
+import java.util.Scanner;
+
+import javax.swing.text.StyledEditorKit.ForegroundAction;
+
+public class String_Java {
+
+	public static void main(String[] args) {
+		//string1
+		System.out.println("Nhap string1: ");
+		Scanner scan = new Scanner(System.in);
+		String string1 = scan.nextLine();
+		
+		// in do dai string1
+		System.out.println(string1.length());
+		// string
+		System.out.println("Nhap string: ");
+	    String string = scan.nextLine();
+	    // kiem tra string co trong string 1
+	    int index = string1.indexOf(string);
+	    System.out.println(index);
+
+	int count = 0;
+	for(int i=0; i<string1.length(); i++) {
+	if(string1.charAt(i)=='A')	{
+		count ++;
+	}
+	}
+	System.out.println("Chuoi string co: " +count +" ki tu 'A'");
+	/*
+	 * cat chuoi String 1 thanh mang String boi khoang trang va ham split for va in
+	 * cac chuoi sau khi cat
+	 */
+	String[] str = string1.split("\\s+");
+	for (String x: str) {
+
+
+		System.out.println("tach chuoi String 1 thanh " + x);
+	}
+}
+}
